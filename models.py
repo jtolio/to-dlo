84a4646c3c107acbfac3ca8477a38a01c5f871a2
jtolds <jtolds@984f1ef2-a355-0410-b46a-0db33d12dc15>
2008-09-27 19:48:46 +0000

    adding categories

--- models.py
+++ models.py
@@ -6,10 +6,6 @@ class ExtendedModel(db.Model):
   def key_id(self):
     return self.key().id()
 
-class Category(ExtendedModel):
-  user = db.UserProperty(required=True)
-  name = db.CategoryProperty(required=True)
-
 class Contact(ExtendedModel):
   user = db.UserProperty(required=True)
   enabled = db.BooleanProperty(required=True, default=False)
@@ -21,12 +17,16 @@ class Phone(Contact):
 class Email(Contact):
   email = db.EmailProperty(required=True)
 
+class Category(ExtendedModel):
+  user = db.UserProperty(required=True)
+  name = db.StringProperty(required=True)
+
 class TodoItem(ExtendedModel):
   user = db.UserProperty(required=True)
   ctime = db.DateTimeProperty(auto_now_add=True, required=True)
   mtime = db.DateTimeProperty(auto_now=True, required=True)
   title = db.StringProperty(required=True, multiline=True)
   body = db.StringProperty(multiline=True)
-  category = db.ReferenceProperty(Category)
   completed = db.BooleanProperty(default=False)
   archived = db.BooleanProperty(default=False)
+  category = db.ReferenceProperty(Category, collection_name="items")
